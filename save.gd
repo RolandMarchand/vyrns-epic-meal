fbdc70c208398e2d7fa302be5f89e6ec13e02988
Moowool <moowool195@gmail.com>
2021-12-09 23:04:45 -0500

    Added rooms missing door combinations. Wrote a function to list all rooms and their opened doors.

--- save.gd
+++ save.gd
@@ -1,6 +1,10 @@
 extends Node
 
 var rooms: Dictionary = {}
+var room_doors: Dictionary = {}
+
+func _ready():
+	_list_room_doors()
 
 func load_room(room: String) -> void:
 	# warning-ignore:return_value_discarded
@@ -14,3 +18,20 @@ func save_room(room: String) -> void:
 		push_warning("Overriding room " + room + " in Save singleton.")
 	Save.rooms[name] = packed_scene
 
+func _list_room_doors():
+	var dir := Directory.new()
+	# warning-ignore:return_value_discarded
+	dir.open("res://Rooms/")
+	# warning-ignore:return_value_discarded
+	dir.list_dir_begin(true, true)
+
+	var room_filename: String = dir.get_next()
+	print(dir.get_current_dir())
+	room_filename = dir.get_current_dir() + room_filename
+
+	while room_filename.trim_prefix("res://Rooms/") != "":
+		room_doors[room_filename] = load(room_filename).instance().neighbors
+		room_filename = dir.get_next()
+		room_filename = dir.get_current_dir() + room_filename
+
+	print("lmao")
